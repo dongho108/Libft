74403d598cade898b86be02f1ed8507e33517da6
dongho <dongho108@naver.com>
2021-11-09 15:31:10 +0900

    initial commit

---
+++ ft_memccpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donghoki <donghoki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 16:54:14 by donghoki          #+#    #+#             */
+/*   Updated: 2021/11/09 15:30:51 by donghoki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+    unsigned char   *p_dest;
+    unsigned char   *p_src;
+    unsigned char   chr;
+    size_t          i;
+    
+    p_dest = dest;
+    p_src = src;
+    chr = c;
+    i = 0;
+    while (i < n)
+    {
+        *p_dest = *p_src;
+        p_dest++;
+        if (*p_src == chr)
+            return (p_dest);
+        p_src++;
+        i++;
+    }
+    return (0);
+}
